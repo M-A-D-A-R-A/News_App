09382ba372c813cdebdcba0e3cf75783480199f4
nishant-madara <andoriyanishant@gmail.com>
2021-08-30 16:31:59 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'core/constants/strings.dart';
+import 'core/themes/app_theme.dart';
+import 'logic/debug/app_bloc_observer.dart';
+import 'presentation/router/app_router.dart';
+
+void main() {
+  Bloc.observer = AppBlocObserver();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: Strings.appTitle,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRouter.home,
+      onGenerateRoute: AppRouter.onGenerateRoute,
+    );
+  }
+}
---
+++ lib/presentation/screens/home_screen/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/constants/strings.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({Key ?key, this.title}) : super(key: key);
+
+  final String ?title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title!),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              Strings.homeScreenCenterText,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
